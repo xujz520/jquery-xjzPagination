b823e1d417466ee2edd9cfd9ff2171a07a5bae80
xujunzhou <765550360@qq.com>
2016-09-08 15:50:29 +0800

    第一次提交

---
+++ docs/javascript.js
@@ -0,0 +1,78 @@
+$(function () {
+    $("#paging1").xjzPagination({
+        totalPages:20,
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+
+
+    $("#paging2").xjzPagination({
+        totalPages: 20,
+        pagingType:'full_numbers',
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+
+
+    $("#paging3").xjzPagination({
+        totalPages: 20,
+        pagingType:'full',
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+
+
+    $("#paging4").xjzPagination({
+        totalPages: 20,
+        pagingType: 'simple',
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+
+
+    $("#paging5").xjzPagination({
+        totalPages: 20,
+        pagingType: 'numbers',
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+
+
+    $("#paging6").xjzPagination({
+        totalPages: 20,
+        visiblePages:1,
+        pagingType: 'full_numbers',
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+
+    $("#paging7").xjzPagination({
+        totalPages: 20,
+        pagingType: 'full_numbers',
+        first: '<<',
+        prev: '<',
+        next: '>',
+        last:'>>',
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+
+
+    $("#paging8").xjzPagination({
+        totalPages: 20,
+        visiblePages: 1,
+        pagingType: 'simple',
+        prevClass: 'PREV',
+        nextClass:'NEXT',
+        onPageClick: function (currentPage) {
+            this.parent().prev().text(currentPage);
+        }
+    });
+});
